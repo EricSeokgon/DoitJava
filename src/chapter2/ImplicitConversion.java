4d7043a7ae591afcb6771ffcf08a2ee2894a3640
hadesyi <leesk55@gmail.com>
2019-01-04 03:38:52 +0900

    continue문

--- src/chapter2/ImplicitConversion.java
+++ src/chapter2/ImplicitConversion.java
@@ -7,5 +7,15 @@ public class ImplicitConversion {
 
         System.out.println(bNum);
         System.out.println(iNum);
+
+        int iNum2 = 20;
+        float fNum = iNum;
+
+        System.out.println(iNum);
+        System.out.println(fNum);
+
+        double dNum;
+        dNum = fNum + iNum;
+        System.out.println(dNum);
     }
 }
