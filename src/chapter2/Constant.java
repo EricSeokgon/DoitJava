45853894b6754d76dc84e158898ccd7fcfbc3fbb
hadesyi <leesk55@gmail.com>
2019-01-04 02:02:43 +0900

    상수 사용하기

---
+++ src/chapter2/Constant.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+public class Constant {
+    public static void main(String[] args) {
+        final int MAX_NUM = 100; // 선언과 동시에 초기화
+        final int MIN_NUM;
+
+        MIN_NUM = 0; //사용하기 전에 초기화, 초기화 하기 않으면 오류 발생
+
+        System.out.println(MAX_NUM);
+        System.out.println(MIN_NUM);
+
+        //MAX_NUM = 1000; //오류 발생. 상수는 값을 변경할 수 없음
+    }
+}
