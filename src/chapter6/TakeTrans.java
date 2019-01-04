b225c234fffdf44ee8b10fa9fc3e041df02ba074
hadesyi <leesk55@gmail.com>
2019-01-05 03:39:17 +0900

    버스와 지하철타기

---
+++ src/chapter6/TakeTrans.java
@@ -0,0 +1,18 @@
+package chapter6;
+
+public class TakeTrans {
+    public static void main(String[] args) {
+        Student studentJames = new Student("James", 5000);
+        Student studentTomas = new Student("Tomas", 10000);
+
+        Bus bus100 = new Bus(100);
+        studentJames.takeBus(bus100);
+        studentJames.showInfo();
+        bus100.showInfo();
+
+        Subway subwayGrteen = new Subway("2호선");
+        studentTomas.takeSubway(subwayGrteen);
+        studentTomas.showInfo();
+        subwayGrteen.showInfo();
+    }
+}
