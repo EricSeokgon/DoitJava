216b4229c665c1129883aeeb7e542f96827f6929
hadesyi <leesk55@gmail.com>
2019-01-06 08:11:31 +0900

    Customer 클래스 구현

---
+++ src/chapter8/Customer.java
@@ -0,0 +1,23 @@
+package chapter8;
+
+public class Customer {
+    private int customerID;
+    private String customerName;
+    private String customerGrade;
+    int bonusPoint;
+    double bonusRatio;
+
+    public Customer() {
+        customerGrade = "SILVER";
+        bonusRatio = 0.01;
+    }
+
+    public int clacPrice(int price) {
+        bonusPoint += price * bonusRatio;
+        return price;
+    }
+
+    public String showCustomerInfo() {
+        return customerName + " 님의 등급은" + customerGrade + "이며, 보너스 포인트는" + bonusPoint + " 입니다.";
+    }
+}
