d56d7dc952722698269928be19f7f4f5bb263b8d
hadesyi <leesk55@gmail.com>
2019-01-06 08:21:04 +0900

    protected 변수 선언

--- src/chapter8/Customer.java
+++ src/chapter8/Customer.java
@@ -20,4 +20,28 @@ public class Customer {
     public String showCustomerInfo() {
         return customerName + " 님의 등급은" + customerGrade + "이며, 보너스 포인트는" + bonusPoint + " 입니다.";
     }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerGrade() {
+        return customerGrade;
+    }
+
+    public void setCustomerGrade(String customerGrade) {
+        this.customerGrade = customerGrade;
+    }
 }
