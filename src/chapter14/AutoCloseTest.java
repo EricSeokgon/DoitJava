d9a64067966eac2975119341a321039bbee8a8b7
hadesyi <leesk55@gmail.com>
2019-01-08 10:15:25 +0900

    try-with-resources문 사용하기 1

---
+++ src/chapter14/AutoCloseTest.java
@@ -0,0 +1,10 @@
+package chapter14;
+
+public class AutoCloseTest {
+    public static void main(String[] args) {
+        try (AutoCloseObj obj = new AutoCloseObj()) {
+        } catch (Exception e) {
+            System.out.println("예외 부분입니다.");
+        }
+    }
+}
